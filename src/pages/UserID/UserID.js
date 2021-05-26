f064a1a6567786b4b4da493f3b8d4b4319bca4b1
Ana Simoes <ana.simoes-mato@unesp.br>
2021-05-26 17:47:59 -0300

    limpando warning

--- src/pages/UserID/UserID.js
+++ src/pages/UserID/UserID.js
@@ -24,10 +24,6 @@ export default function UserID() {
 
 //*/
 useEffect(() => {
-    updateList();
-}, []);
-
-function updateList() {
     const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/${idUser}/posts`, {
         headers: {
             Authorization: `Bearer ${user.token}`,
@@ -42,11 +38,11 @@ function updateList() {
         console.log(error.response.data);
         setIsWaitingServer(false);
         setInternalError(false);
-    });
-} 
+    }); 
+}, [idUser,user.token]);
+
+
    
-
-
     return (
         <Main>
             <Content>
